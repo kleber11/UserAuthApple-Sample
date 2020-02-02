89950690f287ab005665e9547d4c8ee42a1a70d8
Vladyslav Vcherashnii <vladyslav.vcherashnii@gmail.com>
2020-02-01 23:18:44 -0500

    Initial Commit

---
+++ UserAuthApple-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserAuthApple-Sample
+//
+//  Created by Vladyslav Vcherashnii on 01.02.2020.
+//  Copyright © 2020 Vladyslav Vcherashnii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
