3f1241e39750b770bc656625e664724d2988c3a8
Vladyslav Vcherashnii <vladyslav.vcherashnii@gmail.com>
2020-04-19 23:41:55 -0700

    Final result

--- UserAuthApple-Sample/ViewController.swift
+++ UserAuthApple-Sample/ViewController.swift
@@ -7,14 +7,86 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    private let signInButton = ASAuthorizationAppleIDButton(type: .default, style: .black)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupController()
     }
 
+    // MARK: - Logic
+    private func setupController() {
+        signInButton.addTarget(self, action: #selector(authorize), for: .touchUpInside)
+        self.view.addSubview(signInButton)
 
+        signInButton.translatesAutoresizingMaskIntoConstraints = false
+        signInButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        signInButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        signInButton.widthAnchor.constraint(equalToConstant: 250.0).isActive = true
+        signInButton.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+    }
+
+    @objc
+    private func authorize() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+    
+    private func verifyUser() {
+        let provider = ASAuthorizationAppleIDProvider()
+        provider.getCredentialState(forUserID: "Saved user id") { (state, error) in
+            switch state {
+            case .authorized:
+                print("authorized")
+            case .notFound:
+                print("User not found")
+            case .revoked:
+                print("Apple has revoked user")
+            case .transferred:
+                print("Transfered")
+            @unknown default:
+                break
+            }
+        }
+    }
 }
 
+extension ViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential {
+        case let credential as ASAuthorizationAppleIDCredential:
+            let userId = credential.user
+            print("User Identifier: ", userId)
+            
+            if let fullname = credential.fullName {
+                print(fullname)
+            }
+            
+            if let email = credential.email {
+                print("Email: ", email)
+            }
+        default:
+            break
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Error: \(error.localizedDescription)")
+    }
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window ?? UIWindow()
+    }
+}
